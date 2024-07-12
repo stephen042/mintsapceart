cc408dc6cabe4d011a79f3f35389838d389952aa
stephen042 <makuochukwu042@gmail.com>
2024-07-12 12:12:54 +0100

    change of live chat

--- app/account/header.php
+++ app/account/header.php
@@ -38,18 +38,8 @@
     <link rel="stylesheet" href="../content/swiper-bundle.css">
     <link rel="stylesheet" href="../content/style.css">
 
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '4460d305585395c3a9e5a245745961f742efcb2d';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+	<!-- live chat jivo -->
+	<script src="//code.jivosite.com/widget/btuvhpnkIw" async></script>
 
 
 
